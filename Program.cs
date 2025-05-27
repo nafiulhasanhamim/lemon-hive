c035fc8e66713896684e6e280f68a27a43a0f0cb
nafiulhasanhamim <mdnafiulhasanhamim12345@gmail.com>
2025-05-28 01:06:46 +0600

    chore: add automigration configuration on Program.cs

--- Program.cs
+++ Program.cs
@@ -203,6 +203,11 @@ builder.Services.AddSwaggerGen(option =>
 });
 
 var app = builder.Build();
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+    db.Database.Migrate();
+}
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
